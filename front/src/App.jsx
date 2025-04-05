0b7583ba8ffc11b15d073376514858276545f795
6Eur3ka9 <i.osman.akdeniz@gmail.com>
2025-04-05 11:09:29 +0200

    setup front and pre setup back

---
+++ front/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  
+  return (
+    <>
+
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100"> 
+        <h1 className="text-3xl font-bold text-blue-600">Welcome to Vite + React + Tailwind CSS</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
