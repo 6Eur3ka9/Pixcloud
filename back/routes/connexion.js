2cd03003192f0a2153b1a998199ad777ca9134f1
Luna-bf <lburattifayolle@gmail.com>
2025-04-08 12:48:12 +0200

    Finished the logg

--- back/routes/connexion.js
+++ back/routes/connexion.js
@@ -9,12 +9,22 @@ app.get('/users', (req, res) => { //J'attends une requête et une réponse
 
 //Ici on va vérifier si le mot de passe et le nom donnés correspondent à un profil enregistré dans la DB
 app.post('/users/login', async (req, res) => {
-    //On essaye de trouver un utilisateur avec le nom donné
+    //On essaye de trouver un utilisateur avec le nom donné dans la requête
     const user = user.find(user => user.name === req.body.name) //Si un nom d'utilisateur est égal à celui demandé dans la requête, alors...
 
-    //On vérifie quand même si il existe bel et bien
+    //On vérifie quand même si il existe bel et bien :
     if(user == null) {
-        return res.status(400).send('This user doesn\'t exist. Please sign in.')//Si l'utilisateur n'existe pas, on renvoie une erreur (res = response)
+        return res.status(400).send('This user doesn\'t exist. Please sign in.') //Si l'utilisateur n'existe pas, on renvoie une erreur (res = response)
+    }
+    try {
+        //On vérifie ensuite si le mot de passe donné dans la requête est le même que celui enregistré dans le profil (user.password = mot de passe hashé)
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Successfully logged in') //Si c'est le cas, on envoie une réponse positive à l'utilisateur
+        } else {
+            res.send('Incorrect password') //Si c'est pas le cas, on envoie une réponse négative
+        }
+    } catch {
+        res.status(500).send()
     }
 })
 
