08675ba5c74885d35f084ad57c220ed386b41e71
Luna-bf <lburattifayolle@gmail.com>
2025-04-08 11:57:31 +0200

    added some comments

--- back/routes/connexion.js
+++ back/routes/connexion.js
@@ -3,8 +3,8 @@ const auth = require('routes/auth.js')
 //Code si-dessous pris sur un article et modifié (à comprendre)
 app.post("/login", async (req, res) => {
     try {
-     const user = await user.findOne({ email: req.body.email });
-     const comparePassword = await bcrypt.compare(
+     const user = await user.findOne({ email: req.body.email }); //On essaye de trouver un utilisateur avec l'email entré dans l'input
+     const comparePassword = await bcrypt.compare( //Puis on compare son mot de passe
       req.body.password,
       user.password
      );
