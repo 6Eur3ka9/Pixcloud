9ca6d8b410af7b74da630eee3ab556dc70818a14
Luna-bf <lburattifayolle@gmail.com>
2025-04-08 12:03:53 +0200

    restarted from scratch

--- back/routes/connexion.js
+++ back/routes/connexion.js
@@ -1,24 +1,2 @@
-const auth = require('routes/auth.js')
+const express = require('express')
 
-//Code si-dessous pris sur un article et modifié (à comprendre)
-app.post("/login", async (req, res) => {
-    try {
-     const user = await user.findOne({ email: req.body.email }); //On essaye de trouver un utilisateur avec l'email entré dans l'input
-     const comparePassword = await bcrypt.compare( //Puis on compare son mot de passe
-      req.body.password,
-      user.password
-     );
-     if (comparePassword) {
-      const token = generateToken(user);
-      res.status(200).json({
-       token,
-       emailAddress: user.email,
-       password: user.password,
-      });
-     }
-    } catch (err) {
-     res.status(500).json({
-      message: ('User doesn\'t exist. Please sign up')
-     });
-    }
-   });
