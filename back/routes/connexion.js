3ddb0e2dce397072a285fefcbdae18a8814e1595
Luna-bf <lburattifayolle@gmail.com>
2025-04-08 12:17:33 +0200

    continued'

--- back/routes/connexion.js
+++ back/routes/connexion.js
@@ -1,2 +1,10 @@
 const express = require('express')
+const app = express() //L'app va exécuter la function express
 
+const getUser = multer({ storage: storage });
+
+app.get('/users', (req, res) => { //J'attends une requête et une réponse
+    res.json(user)
+})
+
+app.listen(3000) //L'app va s'exécuter sur le serveur 3000
