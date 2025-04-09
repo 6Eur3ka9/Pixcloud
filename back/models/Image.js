4c205c343ba96c6ae9b577fd510ad37aeb9e3b76
Luna-bf <lburattifayolle@gmail.com>
2025-04-09 09:18:50 +0200

    Added new things, commenting helps me

--- back/models/Image.js
+++ back/models/Image.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const db = client.db(Pixcloud);
-const bucket = new mongodb.GridFSBucket(db);
-
 
 //https://www.mongodb.com/docs/drivers/node/current/fundamentals/gridfs/
+
+const bucket = new mongodb.GridFSBucket(db, { 
+    bucketName: 'imagesBucket'
+});
+
+const mongoClient = new mongo.MongoClient(URI, { //Cette constante va être utilisée pour ce connecter à la base de donnée (ici MongoDB) en utilisant l'url de connexion: URI (défini dans un autre endroit du code)
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+fs.createReadStream('./imageFile').
+pipe(bucket.openUploadStream('imageFile', {
+    chunkSizeBytes: 1048576,
+    metadata: { field: 'imageFile', value: 'myValue' }
+}));
 
 const ImageSchema = new Schema({
     image: {
