c5f35d93fa3f560b397336b8203d9abfc9c154a2
Luna-bf <lburattifayolle@gmail.com>
2025-04-09 15:14:01 +0200

    corrected some stuffs in ImageId

--- back/models/ImageId.js
+++ back/models/ImageId.js
@@ -4,10 +4,11 @@ const Schema = mongoose.Schema;
 const ImageIdSchema = new Schema({
     user: {
         image: {
-            type: ['.jpg', '.jpeg', '.png'],
+            type: String,
             _id: id,
             required: true,
             unique: true,
+            match: "/\.(jpg|jpeg|png)$/i",
         },
         uploaded_at: {
             type: Date,
