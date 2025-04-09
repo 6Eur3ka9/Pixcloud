20d1113c8f6a0d36a208fcd497ad1a3bfc5f5547
Luna-bf <lburattifayolle@gmail.com>
2025-04-09 12:07:53 +0200

    Added a few more lines of code

--- back/models/SecondImage.js
+++ back/models/SecondImage.js
@@ -1,4 +1,17 @@
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const path = require('path');
 const crypto = require('crypto');
 const multer = require('multer');
+const gridFsStorage = require('multer-gridfs-storage');
+const grid = require('gridfs-stream');
+const methodOverride = require('method-override');
+
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+
+const connection = mongoose.createConnection(MONGODB_URI); //Connection à la DB
+
+let gfs; //Initialisation de GridFS
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
