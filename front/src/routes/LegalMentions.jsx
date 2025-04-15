4ba3d592fb9ec2f4f9e97dade13d8112fb759644
Luna-bf <lburattifayolle@gmail.com>
2025-04-15 10:39:40 +0200

    Legal Mentions.

--- front/src/routes/LegalMentions.jsx
+++ front/src/routes/LegalMentions.jsx
@@ -10,7 +10,7 @@ function LegalMentions() {
         <div className="bg-white p-5 w-4xl m-auto rounded-lg border-[0.5px]">
             <h1 className="text-center mb-5 font-bold">Mentions légales :</h1>
 
-            <h2>Éditeur et responsable du contenu du site :</h2>
+            <h2 class="mt-8 mb-4">Éditeur et responsable du contenu du site :</h2>
             <p className="mt-4 mb-4">Cette mention est valable pour tous les sites exploités par Pixcloud ainsi que pour tous les réseaux sociaux de Pixcloud.</p>
 
             <h3 className="mt-8 mb-4">Informations sur la société :</h3>
@@ -21,10 +21,10 @@ function LegalMentions() {
                 <li className="mt-3">E-mail : john.doe@gmail.com</li>
             </ul>
 
-            <h3>Numéro d’identification de la société</h3>
-            <p>123456</p>
+            <h3 className="mt-8 mb-4">Numéro d’identification de la société :</h3>
+            <p >123456</p>
 
-            <h3>Direction / représentée par :</h3>
+            <h3 class="mt-8 mb-4">Direction / représentée par :</h3>
             <p>John Doe</p>
 
             <a href="/mainpage" className="flex justify-center text-purple-400 hover:text-purple-600 font-medium mt-5">Revenir à la page principale</a>
