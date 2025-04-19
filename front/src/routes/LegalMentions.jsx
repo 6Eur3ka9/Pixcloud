a1e68bd0d2db395fe6e52dcc076cbec5b202b0d9
Luna-bf <lburattifayolle@gmail.com>
2025-04-19 08:05:56 +0200

    fix navbar

--- front/src/routes/LegalMentions.jsx
+++ front/src/routes/LegalMentions.jsx
@@ -1,8 +1,25 @@
 import mainPage from "../routes/mainPage.jsx"
-import Footer from "../components/footer";
 import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import logo from '../assets/logo.png'
+import { useState, useEffect } from "react";
+import { UserService } from '../service/user.service';
 
 function LegalMentions() {
+
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+        const userid = localStorage.getItem('userId');
+
+        UserService.getUserById(userid)
+          .then((response) => {
+            setUsername(response.data.username);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, []);
 
     return (
     <div className="min-h-screen flex justify-center flex-col bg-gradient-to-b from-[#A406FF] via-[#D580FF] to-transparent">
@@ -35,7 +52,7 @@ function LegalMentions() {
         <div className="bg-white p-5 w-4xl m-auto rounded-lg border-[0.5px]">
             <h1 className="text-center mb-5 font-bold">Mentions légales :</h1>
 
-            <h2 class="mt-8 mb-4">Éditeur et responsable du contenu du site :</h2>
+            <h2 className="mt-8 mb-4">Éditeur et responsable du contenu du site :</h2>
             <p className="mt-4 mb-4">Cette mention est valable pour tous les sites exploités par Pixcloud ainsi que pour tous les réseaux sociaux de Pixcloud.</p>
 
             <h3 className="mt-8 mb-4">Informations sur la société :</h3>
@@ -49,7 +66,7 @@ function LegalMentions() {
             <h3 className="mt-8 mb-4">Numéro d’identification de la société :</h3>
             <p >123456</p>
 
-            <h3 class="mt-8 mb-4">Direction / représentée par :</h3>
+            <h3 className="mt-8 mb-4">Direction / représentée par :</h3>
             <p>John Doe</p>
 
             <a href="/mainpage" className="flex justify-center text-purple-400 hover:text-purple-600 font-medium mt-5">Revenir à la page principale</a>
